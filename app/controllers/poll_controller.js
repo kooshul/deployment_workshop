1798b48c910f101a93f4a903db14c28b2b94bf46
Kooshul Jhaveri <kooshul@gmail.com>
2017-05-11 18:23:09 -0400

    Finished workshop

---
+++ app/controllers/poll_controller.js
@@ -0,0 +1,24 @@
+import Poll from '../models/poll';
+
+export const getPolls = () => {
+  return Poll.find({});
+};
+
+export const createPoll = (poll) => {
+  const p = new Poll();
+  p.text = poll.text;
+  p.imageURL = poll.imageURL;
+  return p.save();
+};
+
+export const vote = (pollID, upvote) => {
+  return Poll.findOne({ _id: pollID }).then((poll) => {
+    console.log(`updating vote: ${poll} ${upvote}`);
+    if (upvote) {
+      poll.upvotes += 1;
+    } else {
+      poll.downvotes += 1;
+    }
+    return poll.save();
+  });
+};
